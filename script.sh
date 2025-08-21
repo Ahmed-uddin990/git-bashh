2e9e8cad4b966b68ee75040762ee89c9e620f1f4
AHMED <mirahmeduddin990@gmail.com>
2025-08-21 13:15:07 +0530

    initial commit

---
+++ script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#This is a comment
+
+NAME="Dilip Still I want to Learn Scripting"
+echo "Hello, $NAME!"
+
+echo "Today is $(date)"
+
+if [[ -f "$file" ]]; then 
+	echo "my file exits"
+else
+	echo "my file missing"
+
+fi
+
+
+
